31e7ae63d48831c9e1d6248b39e7e4f79def7c0f
anst <starininandrey@gmail.com>
2021-11-13 13:32:54 +0300

    Добавление модели данных студента

---
+++ DataModel/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataModel
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public List<string> Phones { get; set; }
+        public string Email { get; set; }
+        public bool IsStudy { get; set; }
+        public string Faculty { get; set; }
+    }
+}
