98a8488a099b7c24cf95c60dc3c4ea94b2729f68
Starinin Andrey <starininandrey@gmail.com>
2021-11-27 12:55:29 +0300

    Добавление чтения данных из БД

--- DataBase.Lib/DataBase.cs
+++ DataBase.Lib/DataBase.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using DataModel;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace DataBase.Lib
 {
     public class DataBase
     {
-        
+        private const string connectionString = "mongodb://localhost:27017";
+        private MongoClient _client;
+
+        public DataBase()
+        {
+            _client = new MongoClient(connectionString);
+        }
+
+        public StudentsList GetAllStudents()
+        {
+            var database = _client.GetDatabase("test");
+            var collection = database.GetCollection<Student>("students");
+            var students = collection.Find(new BsonDocument()).ToList();
+            
+            return new StudentsList(students);
+        }
     }
 }
