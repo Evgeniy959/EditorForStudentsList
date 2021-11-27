c750c4bbc70796a118a235f8d445ad99c5a98eed
Starinin Andrey <starininandrey@gmail.com>
2021-11-27 13:46:34 +0300

    Тестовый проект вывода списка студентов

--- DataModel/Student.cs
+++ DataModel/Student.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace DataModel
 {
     public class Student
     {
+        [BsonId]
+        public ObjectId Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string DateOfBirth { get; set; } //BUG неверный тип данных
         public List<string> Phones { get; set; }
         public string Email { get; set; }
         public bool IsStudy { get; set; }
